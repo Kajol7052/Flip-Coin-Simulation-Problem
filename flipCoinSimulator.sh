bb1c0bac464923e6bbfd3c0f8db1cdfbc1bd9ae7
“Kajol7052” <kajol7052@gmail.com>
2020-11-23 23:30:47 +0530

    Added : Adding an Tie Condition

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -31,4 +31,34 @@ elif [[ $tails -gt $heads ]]
 then
         echo "Tails wins 21 times first"
         echo "Tails wins by $((tails-heads))"
+else
+#UC-4 If match tie then the game continues till the difference of minimum 2 points is achieved
+        diff1=$((heads-tails))
+        diff2=$((tails-heads))
+        while [[ $diff1 -ne 2 && $diff2 -ne 2 ]]
+        do
+                temp=$((1+RANDOM%2)); # randomly generating heads or tails
+                if [ $temp -eq 1 ]      # if temp is equal to 1
+                then
+                heads=$((heads+1))      # then increment heads
+                fi
+                if [ $temp -eq 2 ]      # if temp is equal to 2
+                then
+                tails=$((tails+1))      # then increment tails
+                fi
+        diff1=$((heads-tails))
+        diff2=$((tails-heads))
+done
+
+echo "No. of Heads wins : $heads"
+echo "No. of Tails wins : $tails"
+
+        if [[ $heads -gt $tails ]]
+        then
+                echo "Head wins by 2"
+        elif [[ $tails -gt $heads ]]
+        then
+                echo "Tails wins by 2"
+        fi
 fi
+
